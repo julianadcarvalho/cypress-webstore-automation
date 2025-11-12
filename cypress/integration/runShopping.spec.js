98b09091eb7a71690105c0812f992feaaac09278
julianadcarvalho <julianafsilvaju@gmail.com>
2025-11-12 09:37:25 -0300

    feat: add ci

--- cypress/integration/runShopping.spec.js
+++ cypress/integration/runShopping.spec.js
@@ -6,19 +6,18 @@ import buy from "../support/pages/buy/pesquisarComprar";
 
 describe('shopping', () => {
 
-  it('devem renderizar o browser', () => {
-      browser.abrirBrowser();
-  })
+  it('devem renderizar o browser [@smoke,@ui]', () => {
+    browser.abrirBrowser();
+  });
 
-  it('Clicar em SIGN IN renderizar pagina e clicar CREATE AN ACCOUNT', () => 
-    {
-     register.fazerLogin();
-     register.fazerCadastro();
-     register.fazerLogoutLogin();
-  })
+  it('Clicar em SIGN IN, renderizar página e criar conta [@login,@register]', () => {
+    register.fazerLogin();
+    register.fazerCadastro();
+    register.fazerLogoutLogin();
+  });
 
-  it('Deve pesquisar por item e adicionar no carrinho, aumentando a quantidade de item, posteriomente fazer logout', () => {
+  it('Deve pesquisar item, adicionar ao carrinho e fazer logout [@buy,@cart,@regression]', () => {
     buy.pesquisarAdicionar();
-  })
+  });
 
-})
+});
